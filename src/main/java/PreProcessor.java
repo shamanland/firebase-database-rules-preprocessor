78691bf0c5f13f5114112d2ee96dbac580ed638f
Oleksii Kropachov <o.kropachov@shamanland.com>
2018-03-28 17:59:58 +0300

    Now it's possible to use any name for macro.

--- src/main/java/PreProcessor.java
+++ src/main/java/PreProcessor.java
@@ -1,11 +1,16 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SuppressWarnings("unchecked")
 public class PreProcessor {
+    private static final Pattern pattern = Pattern.compile("\\.(\\{.*})");
+
     public static void main(String[] args) throws Exception {
         Reader in = args.length > 0 ? new InputStreamReader(new FileInputStream(args[0])) : new InputStreamReader(System.in);
         PrintStream out = args.length > 1 ? new PrintStream(args[1]) : System.out;
@@ -27,31 +32,29 @@ public class PreProcessor {
         }
     }
 
-    private static void process(Map<String, Object> map, Map<Character, String> subs) {
+    private static void process(Map<String, Object> map, Map<String, String> subs) {
         List<Map> children = new LinkedList<>();
 
-        Map<Character, String> localSubs = new HashMap<>();
+        Map<String, String> localSubs = new HashMap<>();
 
         for (Map.Entry<String, Object> e : map.entrySet()) {
-            if (e.getKey().startsWith("..")) {
-                if (e.getKey().length() != 3) {
-                    throw new IllegalStateException("Macro '" + e.getKey() + "' is too long");
-                }
+            Matcher m = pattern.matcher(e.getKey());
 
-                localSubs.put(e.getKey().charAt(2), (String) e.getValue());
+            if (m.matches()) {
+                localSubs.put(m.group(1), (String) e.getValue());
             } else if (e.getValue() instanceof Map) {
                 children.add((Map) e.getValue());
             }
         }
 
-        Map<Character, String> mergedSubs = merge(subs, localSubs);
+        Map<String, String> mergedSubs = merge(subs, localSubs);
 
         substitute(map, ".read", mergedSubs);
         substitute(map, ".write", mergedSubs);
         substitute(map, ".validate", mergedSubs);
 
-        for (Character k : localSubs.keySet()) {
-            map.remove(".." + k);
+        for (String k : localSubs.keySet()) {
+            map.remove("." + k);
         }
 
         for (Map m : children) {
@@ -60,30 +63,22 @@ public class PreProcessor {
     }
 
     private static <K, V> Map<K, V> merge(Map<K, V> map1, Map<K, V> map2) {
-        Map<K, V> result = new HashMap<>();
+        Map<K, V> result = new TreeMap<>();
         result.putAll(map1);
         result.putAll(map2);
         return Collections.unmodifiableMap(result);
     }
 
-    private static void substitute(Map<String, Object> map, String key, Map<Character, String> substitutions) {
+    private static void substitute(Map<String, Object> map, String key, Map<String, String> substitutions) {
         Object v = map.get(key);
         if (v instanceof String) {
             String value = (String) v;
 
-            StringBuilder result = new StringBuilder();
-
-            for (int i = 0, n = value.length(); i < n; ++i) {
-                String s = substitutions.get(value.charAt(i));
-
-                if (s != null) {
-                    result.append(s);
-                } else {
-                    result.append(value.charAt(i));
-                }
+            for (Map.Entry<String, String> e : substitutions.entrySet()) {
+                value = StringUtils.replace(value, e.getKey(), e.getValue(), -1);
             }
 
-            map.put(key, result.toString());
+            map.put(key, value);
         }
     }
 
